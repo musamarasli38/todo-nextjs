0f3f968372712783457c50b6627276e5c894a06a
musamaraslinewcode <152208035+Musa-Odisee@users.noreply.github.com>
2024-09-18 23:44:10 +0200

    feature: task form

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,9 +1,16 @@
+import { Task } from "@/components/task";
+
 export default function Home() {
   return (
-    <main>
+    <main className="mx-auto py-4 space-y-3">
       <div>
-        <h1>Todo App</h1>
+        <h1 className="text-3xl text-center">Todo App</h1>
       </div>
+      <Task
+        title="Name your task:"
+        description="Describe your task:"
+        date="To do by:"
+      ></Task>
     </main>
   );
 }
---
+++ todo-app/src/components/task.tsx
@@ -0,0 +1,43 @@
+import { ComponentProps } from "react";
+import { Card, CardContent, CardHeader } from "./ui/card";
+
+type TaskProps = ComponentProps<"div"> & {
+  title: string;
+  description: string;
+  date: string;
+};
+
+export function Task({ title, description, date }: TaskProps) {
+  return (
+    <Card className="px-16 w-full">
+      <CardHeader>
+        <label htmlFor="title">{title}</label>
+        <input
+          type="text"
+          className="border border-gray-300 rounded-lg p-2"
+          name="title"
+        ></input>
+      </CardHeader>
+      <CardContent className=" flex flex-col flex-wrap space-y-3">
+        <label htmlFor="description">{description}</label>
+        <textarea
+          className="border border-gray-300 rounded-lg p-2"
+          rows={4}
+          name="description"
+        ></textarea>
+        <label htmlFor="date">{date}</label>
+        <input
+          type="date"
+          className="border border-gray-300 rounded-lg p-2"
+          name="date"
+        ></input>
+        <button
+          type="submit"
+          className="btn-primary bg-primary text-white rounded-lg self-start px-4 py-2"
+        >
+          Save
+        </button>
+      </CardContent>
+    </Card>
+  );
+}
