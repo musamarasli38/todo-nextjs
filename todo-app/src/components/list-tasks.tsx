c8f128f5bd4c4edaa6d54dc2094c002b93337b9b
musamaraslinewcode <152208035+Musa-Odisee@users.noreply.github.com>
2024-09-25 10:43:53 +0200

    deployment troubleshooting

---
+++ todo-app/src/components/list-tasks.tsx
@@ -0,0 +1,12 @@
+export function ListTasks() {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl text-center">For today:</h1>
+      </div>
+      <div>
+        <h2>For tomorrow:</h2>
+      </div>
+    </div>
+  );
+}
