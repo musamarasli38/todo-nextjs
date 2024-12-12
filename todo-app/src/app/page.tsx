fc73697974f4c51b4cdefc4de5d5ebae31f7a449
musamarasli38 <musa.marasli38@gmail.com>
2024-12-12 21:31:48 +0100

    test

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,7 +1,13 @@
 import ListTasks from "@/components/list-tasks";
+import { TaskForm } from "@/components/task-form";
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
+      <TaskForm
+        onTaskAdded={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      ></TaskForm>
       <ListTasks></ListTasks>
     </div>
   );
